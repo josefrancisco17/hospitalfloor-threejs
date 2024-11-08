d95e2b0019128364a0f352835bffefc5387f3f8e
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-11-08 11:03:27 +0000

    Creating base of building

--- src/main.js
+++ src/main.js
@@ -1,33 +1,53 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+const wallHeight = 3;
+
 const scene = new THREE.Scene();
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
+const axesHelper = new THREE.AxesHelper( 5 );
+axesHelper.position.set(0,10,0);
+scene.add( axesHelper );
 
-const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true,
-})
+//create meshes
+function createFloor(width, depth) {
+    const floorGeometry = new THREE.PlaneGeometry(width, depth);
+    const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc, side: THREE.DoubleSide });
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -Math.PI / 2;
+    scene.add(floor);
+}
 
-const sphereMesh = new THREE.Mesh(geometry, material);
+function createWall(width, height, depth, x, y, z) {
+    const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    const wallGeometry = new THREE.BoxGeometry(width, height, depth);
+    const wall = new THREE.Mesh(wallGeometry, wallMaterial);
+    wall.position.set(x, y, z);
+    scene.add(wall);
+}
 
-scene.add(sphereMesh);
+createWall(50, wallHeight, 0.2, 0, wallHeight / 2, -25);
+createWall(50, wallHeight, 0.2, 0, wallHeight / 2, 25);
+createWall(0.2, wallHeight, 50, -25, wallHeight / 2, 0);
+createWall(0.2, wallHeight, 50, 25, wallHeight / 2, 0);
 
+createFloor(50,50)
+
+//camera
 const camera = new THREE.PerspectiveCamera(
-    35,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     200
 );
+camera.position.set(25, 25, 25);
 
-camera.position.z = 100;
-
+//controls
 const canvas = document.querySelector("canvas.threejs");
-
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+//renderer
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
